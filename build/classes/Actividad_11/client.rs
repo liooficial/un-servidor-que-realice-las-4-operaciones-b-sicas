d4a6fa49c9254fc7336e8e0a4dc29daf365e0c77
liooficial <jjuli123.jm456@gmail.com>
2022-07-11 10:40:05 -0500

    version1

---
+++ build/classes/Actividad_11/client.rs
@@ -0,0 +1,5 @@
+comunicaciones.client
+comunicaciones.client$2
+comunicaciones.client$1
+comunicaciones.client$4
+comunicaciones.client$3
---
+++ build/classes/Actividad_11/serve.rs
@@ -0,0 +1,4 @@
+comunicaciones.serve
+comunicaciones.serve$3
+comunicaciones.serve$2
+comunicaciones.serve$1
